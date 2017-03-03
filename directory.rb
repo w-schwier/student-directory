853dc8e34d45c37bbe71b70dfa8309ffd1bf7a0f
Will <w.schwier@hotmail.co.uk>
2017-03-03 15:12:14 +0000

    Added directory.rb

---
+++ directory.rb
@@ -0,0 +1,17 @@
+puts "The students of Villains Academy"
+puts '-' * 10
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleono"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+
+print "Overall, we have "
+print 11
+puts " great students"
